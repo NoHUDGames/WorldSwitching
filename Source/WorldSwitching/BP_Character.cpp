6bead79a3490ad0f0d8b88481d6424ee32056d3a
Benedikt <bendikorn@gmail.com>
2019-02-03 15:27:27 +0000

    Satt opp karakter
    Vil kunne gjøre kule ting med cameracomponenten

---
+++ Source/WorldSwitching/BP_Character.cpp
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BP_Character.h"
+
+// Sets default values
+ABP_Character::ABP_Character()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABP_Character::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABP_Character::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ABP_Character::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveUp", this, &ABP_Character::MoveUp);
+	PlayerInputComponent->BindAxis("MoveDown", this, &ABP_Character::MoveDown);
+	PlayerInputComponent->BindAxis("MoveLeft", this, &ABP_Character::MoveLeft);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ABP_Character::MoveRight);
+
+}
+
+void ABP_Character::MoveUp(float AxisValue)
+{
+	AddMovementInput(FVector(0.f, -50.f, 0.f), AxisValue);
+}
+
+void ABP_Character::MoveDown(float AxisValue)
+{
+	AddMovementInput(FVector(0.f, 50.f, 0.f), AxisValue);
+}
+void ABP_Character::MoveLeft(float AxisValue)
+{
+	AddMovementInput(FVector(-50.f, 0.f, 0.f), AxisValue);
+}
+void ABP_Character::MoveRight(float AxisValue)
+{
+	AddMovementInput(FVector(50.f, 0.f, 0.f), AxisValue);
+}
---
+++ Source/WorldSwitching/BP_Character.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BP_Character.generated.h"
+
+UCLASS()
+class WORLDSWITCHING_API ABP_Character : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ABP_Character();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	void MoveUp(float AxisValue);
+	void MoveDown(float AxisValue);
+	void MoveLeft(float AxisValue);
+	void MoveRight(float AxisValue);
+};
