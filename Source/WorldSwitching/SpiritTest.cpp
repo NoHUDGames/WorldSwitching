4ccb4e4c94d45071b2874b499b294ce13fa93db8
krismale <kristian.male@yahoo.com>
2019-02-25 16:23:41 +0100

    Optimized the attack of the SpiritEnemy, and started on TempHUD
    Now the attack of the spirit enemy works in the same way as the players attack. The HUD is just temporary, and isn't fully implemented yet. Will display the health and number of artifacts the player is holding

--- Source/WorldSwitching/SpiritTest.cpp
+++ Source/WorldSwitching/SpiritTest.cpp
@@ -1,6 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "SpiritTest.h"
+#include "Components/SceneComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/BoxComponent.h"
 #include "Components/CapsuleComponent.h"
 
 // Sets default values
@@ -8,6 +11,27 @@ ASpiritTest::ASpiritTest()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	WeaponRotation = CreateDefaultSubobject<USceneComponent>(TEXT("WeaponRotation"));
+	WeaponRotation->SetupAttachment(RootComponent);
+	WeaponRotation->Mobility = EComponentMobility::Movable;
+	WeaponRotation->bVisualizeComponent = true;
+
+	WeaponVisual = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TempWeapon"));
+	WeaponVisual->SetupAttachment(WeaponRotation);
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> WeaponVisualAsset(TEXT("/Engine/BasicShapes/Cube.Cube"));
+	if (WeaponVisualAsset.Succeeded())
+	{
+		WeaponVisual->bVisualizeComponent = true;
+		WeaponVisual->SetStaticMesh(WeaponVisualAsset.Object);
+		WeaponVisual->SetRelativeLocation(FVector(0.0f, 0.0f, 50.0f));
+		WeaponVisual->Mobility = EComponentMobility::Movable;
+		WeaponVisual->SetWorldScale3D(FVector(0.2f));
+	}
+
+	/// Weapon collider, the collision sphere that damages the player when using the weapon
+	BoxCollider = CreateDefaultSubobject<UBoxComponent>(TEXT("WeaponCollider"));
+	BoxCollider->SetupAttachment(WeaponVisual);
 
 }
 
--- Source/WorldSwitching/SpiritTest.h
+++ Source/WorldSwitching/SpiritTest.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Components/BoxComponent.h"
 #include "SpiritTest.generated.h"
 
 UCLASS()
@@ -26,11 +28,26 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+
+	/// Start of variables and functions that BP_Character uses to kill the enemy
 	int Lives{ 3 };
 
 	void KillingEnemy();
 
 	void DecrementingLives();
+	/// End of variables and functions that BP_Character uses to kill the enemy
+
+	/// Start of variables and functions for the enemy attack
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		UBoxComponent* BoxCollider { nullptr };
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		USceneComponent* WeaponRotation { nullptr };
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		UStaticMeshComponent* WeaponVisual { nullptr };
+
+	/// End of variables and functions for the enemy attack
 
 
 };
